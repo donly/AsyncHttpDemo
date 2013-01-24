f94f9bf7545b38a76896a7c921fde62fb3b96ab4
donly <donly@magicalboy.com>
2013-01-24 23:36:49 +0800

    Initial Commit

---
+++ AsyncHttpDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AsyncHttpDemo
+//
+//  Created by Chan Donly on 13-1-24.
+//  Copyright (c) 2013年 Diaoser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AsyncHttpDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AsyncHttpDemo
+//
+//  Created by Chan Donly on 13-1-24.
+//  Copyright (c) 2013年 Diaoser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
